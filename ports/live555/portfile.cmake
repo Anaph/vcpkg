2b21ef21146455fbb7067be9919f4c34d1abe83c
Anaph <danilaa001@gmail.com>
2025-04-29 21:47:17 +0300

    Update portfile.cmake

--- ports/live555/portfile.cmake
+++ ports/live555/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 string(REPLACE "-" "." format_version ${VERSION})
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://live555.com/liveMedia/public/live-latest.tar.gz"
+    URLS "http://live555.com/liveMedia/public/live555-latest.tar.gz"
     FILENAME "live.${format_version}.tar.gz"
     SHA512 78cedf67931b0227b7afed7a64c59db1e1dc7bc4ba5a2b7b4cc638bdf3a03c3bc04499bb4d7f5bdd692f53faca6f5a2a33295daf5bba1c153f66977465b61b8e
 )
